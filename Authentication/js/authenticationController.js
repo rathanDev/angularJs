4aa14c89e926f0aaebb90c70f60322fda1737dd3
janarthan <janarthan@finnovation.lk>
2017-09-18 14:23:05 +0530

    Added AuthenticationService

--- Authentication/js/authenticationController.js
+++ Authentication/js/authenticationController.js
@@ -1,21 +1,20 @@
-app.controller('authenticationController', function ($scope) {
+app.controller('AuthenticationController', function ($scope, AuthenticationService) {
 
     $scope.username = "username";
     $scope.password = "password";
 
     $scope.authenticate = function () {
 
-        var username = "admin";
-        var password = "pass";
-
         $scope.success = '';
         $scope.error = '';
+        //
+        // if ($scope.username === username && $scope.password === password) {
+        //     $scope.success = "Authenticated";
+        // } else {
+        //     $scope.error = "Invalid username or password";
+        // }
 
-        if ($scope.username === username && $scope.password === password) {
-            $scope.success = "Authenticated";
-        } else {
-            $scope.error = "Invalid username or password";
-        }
+        AuthenticationService.authenticate($scope.username, $scope.password);
 
     }
 
---
+++ Authentication/js/authenticationService.js
@@ -0,0 +1,15 @@
+app.service('AuthenticationService', function () {
+
+    this.authenticate = function (username, password) {
+
+        this.username = "admin";
+        this.password = "pass";
+
+        if (username === this.username && password === this.password) {
+            console.log("Authenticated");
+        } else {
+            console.log("Invalid username or password");
+        }
+    }
+
+});
