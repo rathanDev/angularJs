0f21e1805867fa735203cf44cf76d3b5c64a7304
janarthan <janarthan@finnovation.lk>
2017-09-18 13:22:37 +0530

    Fixed authentication message

--- Authentication/js/authenticationController.js
+++ Authentication/js/authenticationController.js
@@ -4,14 +4,19 @@ app.controller('authenticationController', function ($scope) {
     $scope.password = "password";
 
     $scope.authenticate = function () {
+
         var username = "admin";
         var password = "pass";
+
+        $scope.success = '';
+        $scope.error = '';
+
         if ($scope.username === username && $scope.password === password) {
             $scope.success = "Authenticated";
-            return;
         } else {
             $scope.error = "Invalid username or password";
         }
+
     }
 
 });
